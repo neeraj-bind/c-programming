d71d13294234010c120f27d08f97f702454d1150
Neeraj Kumar Bind <162834425+neeraj-bind@users.noreply.github.com>
2024-03-11 19:53:08 +0530

    Data types added

---
+++ 2DataTypes.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    int a = 10;
+    float b = 20.5;
+    char c= 'A';
+    printf("The value of a is %d\n", a);
+    printf("The value of b is %.2f\n", b);
+    printf("The value of c is %c\n", c);
+    
+    return 0;
+}
