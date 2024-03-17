27bc5d614bec33bdf84136d43eac9ab64aabf2c3
Neeraj Kumar Bind <162834425+neeraj-bind@users.noreply.github.com>
2024-03-17 15:17:16 +0530

    Create 8ProdOfTwoNum.c

---
+++ 8ProdOfTwoNum.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() 
+{
+    int x, y, result; // Declare variables for two integers and their product
+    
+    // Prompt user for input and store in 'x'
+    printf("\nInput the first integer: "); 
+    scanf("%d", &x);
+    
+    // Prompt user for input and store in 'y'
+    printf("\nInput the second integer: ");
+    scanf("%d", &y);
+    
+    result = x * y; // Calculate the product of 'x' and 'y'
+    
+    // Print the product
+    printf("Product of the above two integers = %d\n", result);
+}
